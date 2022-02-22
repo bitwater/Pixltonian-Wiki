94dcd81c7de1de428a2b7548ebcf8e3c02be8d4e
bitwater <luosanshui@foxmail.com>
2022-02-22 08:44:49 +0800

    modified:   .github/workflows/vuepress-deploy.yml

--- src/.vuepress/.temp/pages/network-data.html.vue
+++
@@ -1,4 +0,0 @@
-<template><h1 id="data" tabindex="-1"><a class="header-anchor" href="#data" aria-hidden="true">#</a> Data</h1>
-<p>Right now, the Pixltonian Network is powered by Pre-Nodes, which are temporarily maintained by the Natural Selection Labs. We constantly upload all Pixltonian Files to a public <a href="https://github.com/NaturalSelectionLabs/Pixltonian-Network-Data" target="_blank" rel="noopener noreferrer">GitHub repository<ExternalLinkIcon/></a>, where everyone can access.</p>
-<p>We also provide a dashboard <a href="https://dashboard.Pixltonian.dev" target="_blank" rel="noopener noreferrer">https://dashboard.Pixltonian.dev<ExternalLinkIcon/></a> to visualize an overview of the current state.</p>
-</template>
